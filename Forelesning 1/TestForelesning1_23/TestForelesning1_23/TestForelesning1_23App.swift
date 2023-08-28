34f4b5a9e9b723f9c91334fd360f840cc72c775c
Håkon Bogen <bogen@beiningbogen.no>
2023-08-28 14:53:29 +0200

    Add lecture 1

---
+++ Forelesning 1/TestForelesning1_23/TestForelesning1_23/TestForelesning1_23App.swift
@@ -0,0 +1,17 @@
+//
+//  TestForelesning1_23App.swift
+//  TestForelesning1_23
+//
+//  Created by Håkon Bogen on 28/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestForelesning1_23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
