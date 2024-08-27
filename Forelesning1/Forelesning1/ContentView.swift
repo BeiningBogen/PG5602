f8d54bd45508e5e586ee962b3a8fd55562f8b76d
Håkon Bogen <bogen@beiningbogen.no>
2024-08-27 13:09:40 +0200

    add first lecture

--- Forelesning1/Forelesning1/ContentView.swift
+++ Forelesning1/Forelesning1/ContentView.swift
@@ -9,13 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .center) {
+           
+            Spacer()
+            
+            
+            HStack {
+                
+                Spacer()
+                
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                
+                ZStack {
+                    Rectangle()
+                        .fill(.blue)
+                        .frame(width: 100, height: 100)
+                        .offset(CGSize(width: 10.0, height: 10.0))
+                        
+                    
+                    Rectangle()
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(.red)
+
+                    Text("Hello, world!")
+                        .foregroundStyle(.white)
+
+                    Text("Second text")
+                        .foregroundStyle(.white)
+                }
+                
+                
+            }
+            .padding(.trailing)
+            
+            Spacer()
+                .padding(.bottom)
+            
         }
-        .padding()
     }
 }
 
